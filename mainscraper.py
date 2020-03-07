c8a1e90383ac095e30ea5bbad863f7e7ca511799
Nathan <44456832+thaniel-c@users.noreply.github.com>
2020-03-07 13:38:30 -0500

    Update mainscraper.py

--- mainscraper.py
+++ mainscraper.py
@@ -14,7 +14,7 @@ address = [] #stores email address for its respective row
 
 
 #Gathering links for each embassy webpage
-filename = 'linksource.txt'
+filename = 'data/linksource.txt'
 f = open(filename, "r")
 hold = f.read().splitlines()
 i = 0;
@@ -31,9 +31,6 @@ for line in hold:
         links.append(temp)
 numberofLinks = len(links)
 
-print(targetraw)
-sleep(5)
-
 #Make chromedriver headless
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -44,13 +41,10 @@ chrome_dir = "/usr/lib/chromium-browser/chromedriver"
 driver = driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_dir)
 driver.get("https://www.google.com")
 
-print(len(links))
-print(len(target))
-print(len(targetnum))
-sleep(5)
 
 #iterate through different embassy links stored
 #scrape each email from each link
+set = False
 for i in range(len(links)):
     driver.get(links[i])
 
@@ -64,12 +58,18 @@ for i in range(len(links)):
     for nation in range(len(targetraw)):
         if(targetraw[nation] in country):
             country = targetraw[nation]
-            target.append(country)
 
     for match in soup.findAll('li', attrs={'id':'email'}):
         print("Fetched email " + match.text[5:] + " " + str(i) + "/" + str(numberofLinks) + " " + str(i/numberofLinks * 100)[:5] + "% for " + country)
         address.append(match.text[5:]) #prevents substring 'Email' from being added to email address string
+        target.append(country)
+
 
 #Package data into .csv file to be read and dealt with accordingly by mailer
+print(str(len(target)) + " | " + str(len(address)))
+
+for f in range(abs(len(address) - len(target))):
+    target.append("blank")
+
 df = pd.DataFrame({'Target':target,'Address':address})
-df.to_csv('data.csv', index=False, encoding='utf-8')
+df.to_csv('data/data.csv', index=False, encoding='utf-8')
